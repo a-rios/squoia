9da741a0ede2115da04cdc87f80a46287e9aa897
arios@ifi.uzh.ch <arios@ifi.uzh.ch@17bc9093-8b4f-c09d-ba2f-0e981a381a31>
2013-03-06 22:14:40 +0000

    MT modules

---
+++ trunk/MT_systems/readConfig.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl
+
+# reads parameter from config file into a hash and writes hash to disk
+# usage: perl readConfig.pl config.cfg
+
+use utf8;
+use open ':utf8';
+use Storable;
+use strict;
+
+
+my $num_args = $#ARGV + 1;
+if ($num_args != 1) {
+  print "\nUsage:  perl readConfig.pl path-to-config\n";
+  exit;
+}
+
+my $confFile = $ARGV[0];
+open CONFIG, "< $confFile" or die "Can't open $confFile : $!";
+
+my %Parameters = ();
+
+while (<CONFIG>) {
+	chomp;       # no newline
+	s/#.*//;     # no comments
+	s/^\s+//;    # no leading white
+	s/\s+$//;    # no trailing white
+	next unless length;    # anything left?
+
+	( my $var, my $value ) = split( /\s*=\s*/, $_, 2 );
+
+	if($var ne 'GRAMMAR_DIR')
+	{
+		my $grammarPath = $Parameters{'GRAMMAR_DIR'} or die "GRAMMAR_DIR not specified in config!";
+		$value =~ s/\$GRAMMAR_DIR/$grammarPath/g;
+
+	}
+	#print "$var=$value\n";
+	$Parameters{$var} = $value;
+	store \%Parameters, 'parameters';
+
+}
+
+close CONFIG;
---
+++ trunk/MT_systems/run_esqu.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# adapt paths to your installation
+export MATXIN_DIR="/opt/matxin/local"
+export FREELINGSHARE=$MATXIN_DIR/share/freeling
+export ESQU_DIR="/home/clsquoia/google_squoia/MT_systems/esqu"
+export GRAMMAR_DIR=$ESQU_DIR/grammar
+export OUTPUT_DIR=$GRAMMAR_DIR/output
+export MATXIN_BIN=$MATXIN_DIR/bin
+
+export FREELING_CONFIG=$FREELINGSHARE/config/es_desr.cfg
+export FREELING_PARAM="-f $FREELING_CONFIG $*"
+export FREELING_PORT="8866"
+export MATXIN_DIX=$ESQU_DIR/lexica/es-quz.bin
+export MATXIN_CONFIG=$GRAMMAR_DIR/es-qu.cfg
+
+# path to desr parser & its model
+export DESR_DIR="/home/clsquoia/parser/desr-1.2.6"
+export DESR_CONFIG=$DESR_DIR/spanishv2.conf
+export DESR_MODEL=$DESR_DIR/spanish_es4.MLP
+#export DESR_MODEL=$DESR_DIR/spanish.MLP
+export DESR_PARAMS="-m $DESR_MODEL -c $DESR_CONFIG"
+
+
+perl readConfig.pl $MATXIN_CONFIG;
+# not server mode (slow!):
+#$MATXIN_BIN/tagFLdesr $FREELING_PARAM | $DESR_DIR/src/desr $DESR_PARAMS | perl conll2xml/conll2xml.pl | perl esqu/disambRelClauses_desr.pl | perl esqu/corefSubj_desr.pl
+
+# server-client mode
+$MATXIN_BIN/analyzer_client $FREELING_PORT |$DESR_DIR/src/desr $DESR_PARAMS | perl conll2xml/conll2xml.pl | perl esqu/disambRelClauses_desr.pl | perl esqu/corefSubj_desr.pl | perl esqu/subordVerbform_desr.pl  | $MATXIN_BIN/matxin-xfer-lex $MATXIN_DIX  | perl splitNodes.pl | perl insertSemanticTags.pl | perl semanticDisamb.pl | perl morphDisamb.pl |xmllint --format - 
+
+
+
