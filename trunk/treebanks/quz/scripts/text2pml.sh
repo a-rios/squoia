ba5c256adf2454c98d8417d7a43304607279d88b
arios@ifi.uzh.ch <arios@ifi.uzh.ch@17bc9093-8b4f-c09d-ba2f-0e981a381a31>
2013-03-06 18:27:48 +0000

    text2pml

--- trunk/treebanks/quz/scripts/text2pml.sh
+++ trunk/treebanks/quz/scripts/text2pml.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-
-export OZLI="/home/ozli"
-export XFST_DIR=$OZLI/mount/xfst_tools/web_analyzer_v2
-export SCRIPT_DIR=$OZLI/squoia/annotation/quechua/scripts
-#export FOMA_DIR=$OZLI/mount/foma_tools/quechua_tools_distributable/spellcheckUnificado_foma
+export PROJECT_DIR="insert-path-to-project"
+export XFST_DIR=$PROJECT_DIR/trunk/morphology/analyzer_xfst
 
 
-
-perl $SCRIPT_DIR/tokenize_withsentPerLine.pl | lookup -flags cKv29TT $XFST_DIR/quechua-web-db.fst | perl $SCRIPT_DIR/truecase_xfst2.pl | perl $SCRIPT_DIR/xfst2pml_v2.pl | perl $SCRIPT_DIR/preAnotate.pl
+perl tokenize_withsentPerLine.pl | lookup -flags cKv29TT $XFST_DIR/quechua-web-db.fst | perl truecase_xfst2.pl | perl xfst2pml_v2.pl | perl preAnotate.pl
 
