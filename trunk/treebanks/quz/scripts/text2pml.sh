b8c1212fd32325acc704b681f7a33d2d7f02605a
arios@ifi.uzh.ch <arios@ifi.uzh.ch@17bc9093-8b4f-c09d-ba2f-0e981a381a31>
2013-04-08 22:33:25 +0000

    info resources

--- trunk/treebanks/quz/scripts/text2pml.sh
+++ trunk/treebanks/quz/scripts/text2pml.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export PROJECT_DIR="insert-path-to-project"
-export XFST_DIR=$PROJECT_DIR/trunk/morphology/analyzer_xfst
+export PROJECT_DIR=/home/ozli/squoia/google_squoia
+export XFST_DIR=$PROJECT_DIR/morphology/analyzer_xfst
 
 
 perl tokenize_withsentPerLine.pl | lookup -flags cKv29TT $XFST_DIR/quechua-web-db.fst | perl truecase_xfst2.pl | perl xfst2pml_v2.pl | perl preAnotate.pl
