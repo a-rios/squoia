6c6f06c48c50c97f58940e571eb9a313cac4694d
arios@ifi.uzh.ch <arios@ifi.uzh.ch@17bc9093-8b4f-c09d-ba2f-0e981a381a31>
2013-06-12 14:39:17 +0000

    morphology disambiguation with crf

---
+++ trunk/morphology/disambiguation/evalCRFwithGold.pl
@@ -0,0 +1,104 @@
+use strict;
+use open ':utf8';
+binmode STDIN, ':utf8';
+binmode STDERR, ':utf8';
+binmode STDOUT, ':utf8';
+
+my $num_args = $#ARGV;
+
+if ( $num_args != 1) {
+  print "\nUsage: perl evalCRFwithGold.pl results.crf gold.crf \n";
+  exit;
+  }
+
+my $crfFile = $ARGV[0];
+my $goldFile = $ARGV[1];
+
+
+open CRF, "< $crfFile" or die "Can't open $crfFile : $!";
+open GOLD, "< $goldFile" or die "Can't open $goldFile : $!";
+#my $crfLine = <CRF>;
+#my $goldLine = <GOLD>;
+#print "$goldLine\n$crfLine\n\n";
+
+my $wcount=0;
+my $correctClass=0;
+my $unknownWords=0;
+my $nbrOfSentences=0;
+my $lines=0;
+
+ while (!eof(CRF) and !eof(GOLD)) {
+ 	  
+ 	  my $crfLine= <CRF>;
+      my $goldLine = <GOLD>;
+      $lines++;
+
+	#if one of the lines is empty, but the other is not-> mismatch in files		
+	if( ($crfLine =~ /^\s*$/) != ($goldLine =~ /^\s*$/) )
+	{
+		die "different sentence splitting at line $lines, cannot compare!\n"
+		
+	}	
+	#skip empty lines (don't count them as words!)
+	elsif($crfLine =~ /^\s*$/ and $goldLine =~ /^\s*$/ )
+	{
+		$nbrOfSentences++;
+		next;
+	}
+	else{
+		 my @rowsCRF = split (/\t|\s/, $crfLine);	 
+		 my @rowsGOLD = split (/\t|\s/, $goldLine);	 
+		 
+		 my $classCRF = @rowsCRF[16];
+		 my $classGOLD = @rowsGOLD[16]; 
+		 #print "$crfLine\n";
+		 #print "\n id:$id word:$word lem:$lem cpos:$cpos pos:$pos info:$info head:$head rel:$rel  \n";
+		
+		#print "$word: $head: $ghead----$rel:$grel\n";
+		$wcount++;
+		if($classCRF eq $classGOLD)
+		{
+			$correctClass++;
+		}
+		# check if first pos in results is ZZZ, in this case, xfst could not analyse the word
+		# -> count those separately for evaluation
+		elsif(@rowsCRF[2] eq 'ZZZ')
+		{
+			$unknownWords++;
+		}
+	}
+
+# check xfst gold standard consistency
+#	else{
+#		my ($word,$rest) = split (/\t|\s/, $crfLine);	 
+#		 my ($word2,$rest2) = split (/\t|\s/, $goldLine);	 
+#		 
+#		if($word ne $word2)
+#		{die "different words: $word vs. $word2, at line $lines\n";}
+#	}
+
+ }
+
+my $correct = ($correctClass/$wcount)*100;
+my $nbrOfwrong = ($wcount-$correctClass); 
+my $wrong = ($nbrOfwrong/$wcount)*100;
+my $truewrong = (($nbrOfwrong-$unknownWords)/$wcount)*100;
+
+
+ 
+print "\n*************************************************\n\n";
+print "   total sentences: $nbrOfSentences\n";
+print "   total words: $wcount\n";
+print "   correct class: ";
+   printf("%.2f", $correct); print "\n";
+print "   wrong class: "; 
+   printf("%.2f", $wrong); print "\n";
+print "   wrong class, xfst failures not considered: "; 
+   printf("%.2f", $truewrong); print "\n\n";
+print "*************************************************\n\n";
+
+
+close CRF;
+close GOLD;
+
+
---
+++ trunk/morphology/disambiguation/splitSentences.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+
+use utf8;                  # Source code is UTF-8
+binmode STDIN, ':utf8';
+binmode STDERR, ':utf8';
+binmode STDOUT, ':utf8';
+use strict;
+use Lingua::Sentence;
+
+my $text;
+while(<>){
+	$text .= " ".$_;
+}
+
+        my $splitter = Lingua::Sentence->new("es", "nonbreaking_prefix.es");
+
+        #my $text = 'Esto es un parráfo. Contiene varias frases. "Pero porqué," me preguntas?';
+
+       # print $splitter->split($text);
+        my @sentences = $splitter->split_array($text);
+        foreach my $s (@sentences){
+        	print $s." #EOS\n";
+        }
+        
+        
