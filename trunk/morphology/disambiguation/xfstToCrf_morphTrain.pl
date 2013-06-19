ce0722b6c14d558b44e51581b2888f5066e0649c
arios@ifi.uzh.ch <arios@ifi.uzh.ch@17bc9093-8b4f-c09d-ba2f-0e981a381a31>
2013-06-19 02:27:01 +0000

    quz morph disamb

--- trunk/morphology/disambiguation/xfstToCrf_morphTrain.pl
+++ trunk/morphology/disambiguation/xfstToCrf_morphTrain.pl
@@ -11,7 +11,7 @@ binmode STDOUT, ':utf8';
 # -test (input to be disambiguated, leave last row empty)
 
 my $num_args = $#ARGV + 1;
-if ($num_args > 1) {
+if ($num_args != 1) {
   print STDERR "\nUsage:  perl xfstToCrf.pl -1/-2/-3\n";	
   print STDERR "-1: NS/VS, -2: nominal+verbal morph disamb, 3: independent suffixes disamb\n";	
   exit;
@@ -127,7 +127,7 @@ if($mode eq '-1')
 				elsif($allmorphs =~ /IPst/ ){$actualClass = "IPst";}
 		}
 		# -sqa
-		elsif(($allmorphs =~ /Perf/ && $string !~ /Cas|Num|Poss/ )|| ($allmorphs =~ /\+IPst/ && $allmorphs !~ /1|2/ ) || $allmorphs =~ /\Q+3.Sg.Subj.IPst\E/)
+		elsif(($allmorphs =~ /Perf/ && $string !~ /Cas|Num/ )|| ($allmorphs =~ /\+IPst/ && $allmorphs !~ /1|2/ ) || $allmorphs =~ /\Q+3.Sg.Subj.IPst\E/)
 		#elsif( $allmorphs =~ /Perf/ || $allmorphs =~ /\+IPst/  || $allmorphs =~ /\Q+3.Sg.Subj.IPst\E/)
 		{
 				push(@possibleClasses, "IPst");
@@ -272,9 +272,11 @@ if($mode eq '-2')
 
 if($mode eq '-3')
 {	
+	# @word: 0: $form (string), 1: $analyses (arrayref) , 2: $possibleClasses (arrayref), 3: $actualClass (string), 4: $sentenceHasEvid (boolean) 5: $precedingGenitive (boolean)
 	# check remaining ambiguities
 	# disambiguate indepenent suffixes 
-	foreach my $word (@words){
+	for(my $i=0;$i<scalar(@words);$i++){
+		my $word = @words[$i];
 		my $analyses = @$word[1];
 		my @possibleClasses = ();
 		my $actualClass;
@@ -282,13 +284,28 @@ if($mode eq '-3')
 		my $string = @$analyses[0]->{'string'};
 		
 			# -n
-			if( ($allmorphs =~ /\Q+3.Sg.Poss\E/ && $string !~ /3\.Sg\.Poss.*(Cas|Pl)/ ) || ($allmorphs =~ /\Q+DirE\E/  && $string =~ /n\[Amb/ && $string !~ /Cas|Num.+DirE/) )
+			if( ($allmorphs =~ /\Q+3.Sg.Poss\E/ && $string !~ /3\.Sg\.Poss.*(Cas|Pl|Top)/ ) || ($allmorphs =~ /\Q+DirE\E/  && $string =~ /n\[Amb/ && $string !~ /(Cas|Num).+DirE/) )
 			#if( $allmorphs =~ /\Q+3.Sg.Poss\E/ || $allmorphs =~ /\Q+DirE\E/  && $string =~ /n\[Amb/  )
 			{
 				push(@possibleClasses, "DirE");
 				push(@possibleClasses, "Poss");
 				if($allmorphs =~  /DirE/){$actualClass = "DirE";}
 				elsif($allmorphs =~ /Poss/ ){$actualClass = "Poss";}
+				# check if sentence already contains an evidential suffix
+				@$word[4] =  &sentenceHasEvid(\@words, $i);
+				#print "@$word[0], has evid: ".&sentenceHasEvid(\@words, $i)."\n";
+				# check if preceding word has a genitive suffix
+				unless($i==0){
+					my $preword = @words[$i-1];
+					my $preanalyses =  @$preword[1];
+					if(@$preanalyses[0]->{'allmorphs'} =~ /\+Gen/){
+						@$word[5] = 1;
+					}
+					else{
+						@$word[5] = 0;
+					}
+				}
+				#print "@$word[0]: evid @$word[4], gen: @$word[5] \n";
 			}
 			# -pis
 			elsif($allmorphs =~ /\Q+Loc+IndE\E/ || ($allmorphs =~ /\Q+Add\E/ && $string !~ /Add.*(IndE|DirE|Asmp)/) )
@@ -298,6 +315,7 @@ if($mode eq '-3')
 				push(@possibleClasses, "Add");
 				if($allmorphs =~ /\Q+Loc+IndE\E/){$actualClass = "Loc_IndE";}
 				elsif($allmorphs =~ /Add/ ){$actualClass = "Add";}
+				@$word[4] =  &sentenceHasEvid(\@words, $i);
 			}
 			# -s with Spanish roots: Plural or IndE (e.g. derechus)
 			elsif($allmorphs =~ /\QNRootES][^DB][--]s[Amb][+IndE]\E/ && $string =~ /s\[NRootES/ )
@@ -305,7 +323,9 @@ if($mode eq '-3')
 				push(@possibleClasses, "Pl");
 				push(@possibleClasses, "IndE");
 				if($allmorphs =~ /\Q+IndE\E/){$actualClass = "IndE";}
-				else{$actualClass = "Pl";}	
+				else{$actualClass = "Pl";}
+				# check if sentence already contains an evidential suffix
+				@$word[4] =  &sentenceHasEvid(\@words, $i);
 			}
 			# else: lexical ambiguities, leave
 			else
@@ -316,8 +336,11 @@ if($mode eq '-3')
 				#$actualClass = @$analyses[0]->{'pos'};
 			}
 	
-		push(@$word, \@possibleClasses);
-		push(@$word, $actualClass);
+		#push(@$word, \@possibleClasses);
+		#push(@$word, $actualClass);
+		@$word[2] = \@possibleClasses;
+		@$word[3] = $actualClass;
+	
 		#print @$word[0].": @possibleClasses\n";
 		#print "actual: $actualClass\n\n";
 	}
@@ -325,74 +348,6 @@ if($mode eq '-3')
 
 my $lastlineEmpty=0;
 
-# print all words, unambiguous ones with class 'none'
-#foreach my $word (@words){
-#	my $analyses = @$word[1];
-#	my $form = @$word[0];
-#	my $possibleClasses = @$word[2];
-#	my $correctClass = @$word[3];
-#	
-#	if($form eq '#EOS' ){
-#		unless($lastlineEmpty == 1){
-#			print "\n";
-#			$lastlineEmpty =1;
-#			next;
-#		}
-#	}
-#	else
-#	{
-#		print "$form\t";
-#		$lastlineEmpty =0;
-#		# uppercase/lowercase?
-#
-##		elsif(substr($form,0,1) eq uc(substr($form,0,1))){
-##			print "uc\t";
-##		}
-##		# lowercase
-##		else{
-##			print "lc\t";
-##		}
-#		print @$analyses[0]->{'pos'}."\t";
-#
-#
-#		my $nbrOfClasses =0;
-#		# possible classes
-#		foreach my $class (@$possibleClasses){
-#			print "$class\t";
-#			$nbrOfClasses++;
-#		}
-#		
-#		while($nbrOfClasses<4){
-#			print "ZZZ\t";
-#			$nbrOfClasses++;
-#		}
-#
-#		#possible morph tags: take ALL morph tags into account 
-#		my $printedmorphs='';
-#		my $nbrOfMorph =0;
-#		foreach my $analysis (@$analyses){
-#			my $morphsref = $analysis->{'morph'};
-#			#print $morphsref;
-#			foreach my $morph (@$morphsref){
-#			unless($printedmorphs =~ /\Q$morph\E/){
-#			print "$morph\t";
-#				$printedmorphs = $printedmorphs.$morph;
-#				$nbrOfMorph++;
-#				}
-#			}
-#		}
-#		while($nbrOfMorph<10){
-#			print "ZZZ\t";
-#			$nbrOfMorph++;
-#		}
-#	
-#		
-#		print "$correctClass";
-#
-#	
-#		print "\n";
-#	}
-#}
 
 # print only ambiguous words, with context as features
 
@@ -405,6 +360,7 @@ for (my $i=0;$i<scalar(@words);$i++){
 	
    if(scalar(@$possibleClasses)>1){
 		print "$form\t";
+   
 
 		print @$analyses[0]->{'pos'}."\t";
 
@@ -529,10 +485,140 @@ for (my $i=0;$i<scalar(@words);$i++){
 					}
 				}
 			}
+		# for morph3: add info about evidential and genitive suffixes 
+		if($mode eq '-3'){
+			if(@$word[4] eq ''){
+				print "ZZZ\t";
+			}
+			else{
+				print "@$word[4]\t";
+			}
+			if(@$word[5] eq ''){
+				print "ZZZ\t";
+			}
+			else{
+				print "@$word[5]\t";
+			}
+			
+		}
 		print "$correctClass";
 		print "\n\n";
 	}
 }
+
+sub sentenceHasEvid{
+	my $wordsref = $_[0];
+	my @words = @$wordsref;
+	my $i = $_[1];
+	
+	my $word = @words[$i];
+	
+	my $analyses = @$word[1];
+	my $form = @$word[0];
+	my $allmorphs = @$analyses[0]->{'allmorphs'};
+	
+	my $j = $i-1;
+	my $prestring = $form;
+	#print "word: $prestring\n";
+	while($prestring !~ /\[\$/){
+		my $preword = @words[$j];
+		my $preanalyses = @$preword[1];
+		$prestring = @$preanalyses[0]->{'string'};
+		my $preallmorphs = @$preanalyses[0]->{'allmorphs'};
+		if($preallmorphs =~ /DirE|IndE|Asmp/ && $prestring!~ /hinaspan/ ){
+			#print "found $prestring, $preallmorphs\n";
+			return 1;
+		}
+		$j--;
+	}
+	my $j = $i+1;
+	my $poststring = $form;
+	#print "word: $poststring\n";
+	while($poststring !~ /\[\$/){
+		my $postword = @words[$j];
+		my $postanalyses = @$postword[1];
+		$poststring = @$postanalyses[0]->{'string'};
+		my $postallmorphs = @$postanalyses[0]->{'allmorphs'};
+		if($postallmorphs =~ /DirE|IndE|Asmp/ && $poststring !~ /hinaspan/  ){
+			#print "found $poststring, $postallmorphs\n";
+			return 1;
+		}
+		$j++;
+	}
+	#print "\n";
+	return 0;
+}
+
+
+# print all words, unambiguous ones with class 'none'
+#foreach my $word (@words){
+#	my $analyses = @$word[1];
+#	my $form = @$word[0];
+#	my $possibleClasses = @$word[2];
+#	my $correctClass = @$word[3];
+#	
+#	if($form eq '#EOS' ){
+#		unless($lastlineEmpty == 1){
+#			print "\n";
+#			$lastlineEmpty =1;
+#			next;
+#		}
+#	}
+#	else
+#	{
+#		print "$form\t";
+#		$lastlineEmpty =0;
+#		# uppercase/lowercase?
+#
+##		elsif(substr($form,0,1) eq uc(substr($form,0,1))){
+##			print "uc\t";
+##		}
+##		# lowercase
+##		else{
+##			print "lc\t";
+##		}
+#		print @$analyses[0]->{'pos'}."\t";
+#
+#
+#		my $nbrOfClasses =0;
+#		# possible classes
+#		foreach my $class (@$possibleClasses){
+#			print "$class\t";
+#			$nbrOfClasses++;
+#		}
+#		
+#		while($nbrOfClasses<4){
+#			print "ZZZ\t";
+#			$nbrOfClasses++;
+#		}
+#
+#		#possible morph tags: take ALL morph tags into account 
+#		my $printedmorphs='';
+#		my $nbrOfMorph =0;
+#		foreach my $analysis (@$analyses){
+#			my $morphsref = $analysis->{'morph'};
+#			#print $morphsref;
+#			foreach my $morph (@$morphsref){
+#			unless($printedmorphs =~ /\Q$morph\E/){
+#			print "$morph\t";
+#				$printedmorphs = $printedmorphs.$morph;
+#				$nbrOfMorph++;
+#				}
+#			}
+#		}
+#		while($nbrOfMorph<10){
+#			print "ZZZ\t";
+#			$nbrOfMorph++;
+#		}
+#	
+#		
+#		print "$correctClass";
+#
+#	
+#		print "\n";
+#	}
+#}
+
 
 #	old version	
 #		# print context words (preceding)
