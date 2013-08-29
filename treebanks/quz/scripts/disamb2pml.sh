dfa8b8a69437e59a395fb841306ba15b67482f69
arios <arios@ifi.uzh.ch>
2013-08-29 22:08:46 +0000

    treebanks

--- treebanks/quz/scripts/disamb2pml.sh
+++ treebanks/quz/scripts/disamb2pml.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export PROJECT_DIR=/home/ozli/squoia/google_squoia
-export XFST_DIR=$PROJECT_DIR/morphology/analyzer_xfst
+#export XFST_DIR=$PROJECT_DIR/morphology/analyzer_xfst
 
 
 perl truecase_xfst2.pl | perl xfst2pml_v2.pl | perl preAnotate.pl
