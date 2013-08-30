ed6e40db55fac4f584bc1ffd00fa7db7a762c148
arios@ifi.uzh.ch <arios@ifi.uzh.ch@17bc9093-8b4f-c09d-ba2f-0e981a381a31>
2013-08-30 09:54:30 +0000

    tokenization

---
+++ trunk/morphology/normalizer/tokenize.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+
+use strict;
+use utf8;
+use open ':utf8';
+binmode STDIN, ':utf8';
+binmode STDERR, ':utf8';
+binmode STDOUT, ':utf8';
+use Uplug::PreProcess::Tokenizer;
+
+my $tokenizer = new Uplug::PreProcess::Tokenizer( lang => 'es' );
+
+my $inStr = do { local $/; <STDIN> };
+# escape glottalization ', so words will be splitted
+$inStr =~ s/(\w)\'(\w)/\1GLOTTALIZE\2/g;
+$inStr =~ s/(\w)\'(\w)/\1GLOTTALIZE\2/g;
+my @tokens = $tokenizer->tokenize( $inStr);
+
+#foreach my $t (@tokens){
+#	print $t."\n";
+#}
+for(my $i=0; $i<scalar(@tokens);$i++) {
+	my $t = @tokens[$i];
+	if($t eq '#' and @tokens[$i+1] eq 'EOS'){
+		print "#EOS\n";
+		$i++;
+	}
+	else{
+		$t =~ s/GLOTTALIZE/'/g;
+		print $t."\n";
+	}
+}
