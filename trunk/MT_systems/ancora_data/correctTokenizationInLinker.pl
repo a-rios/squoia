a5393992256879ff15c376c1c20ca31a7edf67e8
arios@ifi.uzh.ch <arios@ifi.uzh.ch@17bc9093-8b4f-c09d-ba2f-0e981a381a31>
2013-11-01 16:54:27 +0000

    xpath logic; node attachment

--- trunk/MT_systems/ancora_data/correctTokenizationInLinker.pl
+++ trunk/MT_systems/ancora_data/correctTokenizationInLinker.pl
@@ -15,20 +15,21 @@ require "$path/../util.pl";
 
 
 #read xml from STDIN
-my $parser = XML::LibXML->new({encoding => 'utf-8'});
+#my $parser = XML::LibXML->new({encoding => 'utf-8'});
 my $dom    = XML::LibXML->load_xml( IO => *STDIN);
 
 my @sentenceList = $dom->getElementsByTagName('SENTENCE');
 
 foreach my $sentence (@sentenceList)
 {
-		my @linkerSP  = $sentence->findnodes('descendant::CHUNK[(@type="grup-sp" or @type="coor-sp") and NODE[@lem="para"] and CHUNK[@type="coor-v" or @type="grup-verb" and NODE[@lem="que" and @mi="CS"]]  ] ');
+		my @linkerSP  = $sentence->findnodes('descendant::CHUNK[(@type="grup-sp" or @type="coor-sp") and NODE[@lem="para"] and CHUNK[(@type="coor-v" or @type="grup-verb") and descendant::NODE[@lem="que" and @mi="CS"]]  ] ');
 		foreach my $linker (@linkerSP){
 			
 			my $linkerParent = $linker->parentNode();
-			$linkerParent->removeChild($linkerParent);
+			#$linkerParent->removeChild($linkerParent);
+			$linkerParent->removeChild($linker);
 			
-			my $verbChild = @{$linker->findnodes( 'CHUNK[@type="coor-v" or @type="grup-verb" and NODE[@lem="que" and @mi="CS"]][1]' )}[0];
+			my $verbChild = @{$linker->findnodes( 'CHUNK[(@type="coor-v" or @type="grup-verb") and descendant::NODE[@lem="que" and @mi="CS"]][1]' )}[0];
 			my ($QUE) = $verbChild->findnodes('NODE/NODE[@lem="que" and @mi="CS"]');
 			$QUE->setAttribute('lem', 'para_que');
 			$QUE->setAttribute('form', 'para_que');
@@ -49,18 +50,16 @@ foreach my $sentence (@sentenceList)
 			print STDERR $verbChild->toString."\n";
 		}
 		
-		#my @linkerADV  = $sentence->findnodes('descendant::CHUNK[(@type="sadv" or @type="coor-sadv") and NODE[@lem="mientras"] and CHUNK[@type="coor-v" or @type="grup-verb" and NODE[@lem="que" and @mi="CS"]]  ] ');
-			
-		my @linkerADV  = $sentence->findnodes('descendant::CHUNK[@type="sadv" or @type="coor-sadv"  ] ');
-		
+		my @linkerADV  = $sentence->findnodes('descendant::CHUNK[(@type="sadv" or @type="coor-sadv") and NODE[@lem="mientras"] and CHUNK[(@type="coor-v" or @type="grup-verb") and descendant::NODE[@lem="que" and @mi="CS"]]  ] ');		
 		foreach my $linker (@linkerADV)
 		{
-			print $linker->toString()."\n";
+			#print $linker->toString()."\n";
 			my $linkerParent = $linker->parentNode();
-			$linkerParent->removeChild($linkerParent);
+			#$linkerParent->removeChild($linkerParent);
+			$linkerParent->removeChild($linker);
 			
 
-			my $verbChild = @{$linker->findnodes( 'CHUNK[@type="coor-v" or @type="grup-verb" and NODE[@lem="que" and @mi="CS"]][1]' )}[0];
+			my $verbChild = @{$linker->findnodes( 'CHUNK[(@type="coor-v" or @type="grup-verb") and descendant::NODE[@lem="que" and @mi="CS"]][1]' )}[0];
 			my ($QUE) = $verbChild->findnodes('NODE/NODE[@lem="que" and @mi="CS"]');
 			$QUE->setAttribute('lem', 'mientras_que');
 			$QUE->setAttribute('form', 'mientras_que');
@@ -75,11 +74,12 @@ foreach my $sentence (@sentenceList)
 				}
 			}
 			
+			$linkerParent->appendChild($verbChild);
 			print STDERR $linker->getAttribute('ord')."\n";
 			print STDERR $verbChild->getAttribute('ord')."\n";
 			print STDERR $verbChild->toString."\n";
 		}
 }
 my $docstring = $dom->toString(3);
-#print $dom->actualEncoding();
-#print STDOUT $docstring;
+print $dom->actualEncoding();
+print STDOUT $docstring;
