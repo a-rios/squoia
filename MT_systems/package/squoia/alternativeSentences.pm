b2af4cdca981ccbd7e089b84f5c649206a77aaca
arios <arios@ifi.uzh.ch>
2014-05-23 11:49:51 +0000

    translate options

--- MT_systems/package/squoia/alternativeSentences.pm
+++ MT_systems/package/squoia/alternativeSentences.pm
@@ -55,6 +55,31 @@ sub addClonedSentence{
 	return $newSent;
 }
 
+sub countAlternatives{
+	my $dom = ${$_[0]};
+
+	my @sentences = $dom->findnodes('//SENTENCE');
+	my $nofsent = int(@sentences);
+	print STDERR "$nofsent sentences before duplication\n";
+	my $totsent;
+	foreach my $sent (@sentences) {
+		my $nofalt = 1;		# number of alternatives for this sentence
+		# get all nodes (NODE) of a sentence with ambigous translations (SYN)
+		foreach my $node ( $sent->findnodes('descendant::NODE[SYN]')) {
+		
+			# count the number of alternatives (SYN) for this node
+			my $nofsyn = int(@{$node->findnodes('SYN')});
+
+			# combinatorial multiplication of alternatives
+			$nofalt = $nofalt * $nofsyn;
+		}
+		my $sref = $sent->getAttribute('ref');
+		print STDERR "$nofalt alternatives for sentence $sref\n";
+		$totsent = $totsent + $nofalt;
+	}
+	print STDERR "$totsent sentences after duplication\n";
+}
+
 sub main{
 	my $dom = ${$_[0]};
 
