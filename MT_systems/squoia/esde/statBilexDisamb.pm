3315e1740c1a31539f543d90a067458f3416e273
arios <arios@ifi.uzh.ch>
2014-09-08 15:31:46 +0000

    de

--- MT_systems/squoia/esde/statBilexDisamb.pm
+++ MT_systems/squoia/esde/statBilexDisamb.pm
@@ -79,95 +79,96 @@ sub main{
 					}
 					else {
 						my $lem = $syn->getAttribute('lem');
+						$lem =~ s/\|//;		# get rid of the "|" in the verbs with separable prefix
 						print STDERR "syn $lem with prob ". $bilexProb{"$slem\t$lem"}." removed\n" if $verbose;
 						$node->removeChild($syn);
 					}
 				}
 		
-		}
-		else {
-			# if more alternatives than max then select the best
-			if ($nofsyn > $maxalt) {
-				# correct the number of alternatives
-				$nofalt = $nofalt * $maxalt / $nofsyn;
+			}
+			else {
+				# if more alternatives than max then select the best
+				if ($nofsyn > $maxalt) {
+					# correct the number of alternatives
+					$nofalt = $nofalt * $maxalt / $nofsyn;
 
-				print STDERR "there are $nofsyn alternatives for the node $slem\n" if $verbose;
-				my $inputalts;
-				my @synonyms = $node->findnodes('SYN');
-				foreach my $syn (@synonyms) {
-					my $lem = $syn->getAttribute('lem');
-					$lem =~ s/\|//;		# get rid of the "|" in the verbs with separable prefix
-					$inputalts .= $lem;
-					if ($lempos) {
-						my $pos = $syn->getAttribute('pos');
-						$inputalts .= "_$pos";
-					}
-					$inputalts .= "\n";
-				}
-				print STDERR "input alternatives:\n$inputalts" if $verbose;
-				# get the scores
-				my $returnstring = `echo '$inputalts' | query $lm null 2>/dev/null | grep "OOV: 0" | sort -k5 | cut -d"=" -f1 | head -$nofalt`;
-				print STDERR "returned string:\n$returnstring" if $verbose;
-				my @selectedalts = split /\n/, $returnstring;
-				my $index;
-				#foreach my $alter (@selectedalts) {
-				#	$index++;
-				#	print STDERR "$index $alter\n";
-				#}
-				# select the first "maxalt" alternatives
-				my %selsyn = ();
-				if (scalar(@selectedalts)) {
-					print STDERR "keep only the selected alternatives\n" if $verbose;
-					for (my $i;$i<$maxalt;$i++) {
-						print STDERR $i+1 ." $selectedalts[$i]\n" if $verbose;
-						$selsyn{$selectedalts[$i]} = $i+1;
-					}
-					# delete the attributes of the first SYN child that have been "copied" into the parent NODE
-					my $firstsyn = $synonyms[0];
-					my @synattrlist = $firstsyn->attributes();
-					foreach my $synattr (@synattrlist)
-					{
-						$node->removeAttribute($synattr->nodeName);
-					}
-					# remove the other synonyms
+					print STDERR "there are $nofsyn alternatives for the node $slem\n" if $verbose;
+					my $inputalts;
+					my @synonyms = $node->findnodes('SYN');
 					foreach my $syn (@synonyms) {
 						my $lem = $syn->getAttribute('lem');
-						$lem =~ s/\|//;
-						if (exists($selsyn{$lem})) {
-							print STDERR "+ $lem\n" if $verbose;
-							# set the node to the first choice
-							if ($lem eq $selectedalts[0]) {
-								print STDERR "************\n" if $verbose;
-								my @attributelist = $syn->attributes();
-								foreach my $attribute (@attributelist)
-								{
-									my $val = $attribute->getValue();
-									my $attr = $attribute->nodeName;
-									$node->setAttribute($attr,$val);
-								}						
+						$lem =~ s/\|//;		# get rid of the "|" in the verbs with separable prefix
+						$inputalts .= $lem;
+						if ($lempos) {
+							my $pos = $syn->getAttribute('pos');
+							$inputalts .= "_$pos";
+						}
+						$inputalts .= "\n";
+					}
+					print STDERR "input alternatives:\n$inputalts" if $verbose;
+					# get the scores
+					my $returnstring = `echo '$inputalts' | query $lm null 2>/dev/null | grep "OOV: 0" | sort -k5 | cut -d"=" -f1 | head -$nofalt`;
+					print STDERR "returned string:\n$returnstring" if $verbose;
+					my @selectedalts = split /\n/, $returnstring;
+					my $index;
+					#foreach my $alter (@selectedalts) {
+					#	$index++;
+					#	print STDERR "$index $alter\n";
+					#}
+					# select the first "maxalt" alternatives
+					my %selsyn = ();
+					if (scalar(@selectedalts)) {
+						print STDERR "keep only the selected alternatives\n" if $verbose;
+						for (my $i;$i<$maxalt;$i++) {
+							print STDERR $i+1 ." $selectedalts[$i]\n" if $verbose;
+							$selsyn{$selectedalts[$i]} = $i+1;
+						}
+						# delete the attributes of the first SYN child that have been "copied" into the parent NODE
+						my $firstsyn = $synonyms[0];
+						my @synattrlist = $firstsyn->attributes();
+						foreach my $synattr (@synattrlist)
+						{
+							$node->removeAttribute($synattr->nodeName);
+						}
+						# remove the other synonyms
+						foreach my $syn (@synonyms) {
+							my $lem = $syn->getAttribute('lem');
+							$lem =~ s/\|//;
+							if (exists($selsyn{$lem})) {
+								print STDERR "+ $lem\n" if $verbose;
+								# set the node to the first choice
+								if ($lem eq $selectedalts[0]) {
+									print STDERR "************\n" if $verbose;
+									my @attributelist = $syn->attributes();
+									foreach my $attribute (@attributelist)
+									{
+										my $val = $attribute->getValue();
+										my $attr = $attribute->nodeName;
+										$node->setAttribute($attr,$val);
+									}						
+								}
+							}
+							else {
+								print STDERR "- $lem\n" if $verbose;
+								$node->removeChild($syn);
 							}
 						}
-						else {
-							print STDERR "- $lem\n" if $verbose;
-							$node->removeChild($syn);
+					}
+					else {
+						# back-off: take the first "random" synonyms
+						print STDERR "back-off: take the first random synonyms\n" if $verbose;
+						for (my $i;$i<$maxalt;$i++) {
+							print STDERR "++ " . $i+1 ." " . $synonyms[$i]->getAttribute('lem') ."\n" if $verbose;
+							$selsyn{$synonyms[$i]} = $i+1;
 						}
+						# remove the other synonyms
+						for (my $i=$maxalt;$i<scalar(@synonyms);$i++) {
+							print STDERR "- ".$synonyms[$i]->getAttribute('lem')."\n" if $verbose;
+							$node->removeChild($synonyms[$i]);
+						}			
 					}
-				}
-				else {
-					# back-off: take the first "random" synonyms
-					print STDERR "back-off: take the first random synonyms\n" if $verbose;
-					for (my $i;$i<$maxalt;$i++) {
-						print STDERR "++ " . $i+1 ." " . $synonyms[$i]->getAttribute('lem') ."\n" if $verbose;
-						$selsyn{$synonyms[$i]} = $i+1;
-					}
-					# remove the other synonyms
-					for (my $i=$maxalt;$i<scalar(@synonyms);$i++) {
-						print STDERR "- ".$synonyms[$i]->getAttribute('lem')."\n" if $verbose;
-						$node->removeChild($synonyms[$i]);
-					}			
 				}
 			}
-		}
 		}
 		my $sref = $sent->getAttribute('ref');
 		print STDERR "$nofalt alternatives for sentence $sref\n" if $verbose;
